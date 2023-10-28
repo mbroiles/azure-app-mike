3d2aa4439639e6bc385ce08b1d97aed678fd3ef5
Michael Broiles <michaelbroiles@yahoo.com>
2023-10-28 10:11:49 -0400

    Added EF Core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace azure_app_mike.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
